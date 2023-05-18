14e8a489414007ab54501a117b80df1e190a2d43
Iasmin Borges <iasmin.borges.soares@gmail.com>
2023-05-18 17:54:06 -0300

    new comments conclusion

--- Type-Script/typescript-curso-1/app/models/negociacoes.ts
+++ Type-Script/typescript-curso-1/app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
-    private negociacoes: Negociacao[] = [];
+    private negociacoes: Array<Negociacao> = []; //OU Negociacao[] = []
 
     adiciona(negociacao: Negociacao) {
-        this.negociacoes.push(negociacao);
+        this.negociacoes.push(negociacao); //push add itens na lista
     }
 
-    lista(): readonly Negociacao[] {
+    lista(): ReadonlyArray<Negociacao> { //OU só readonly define que a lista é somente leitura
         return this.negociacoes;
     }
 }
