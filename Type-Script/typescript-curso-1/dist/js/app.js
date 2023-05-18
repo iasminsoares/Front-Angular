c62ae09e59890ccf0340fbd9718f98870e57b62a
Iasmin Borges <iasmin.borges.soares@gmail.com>
2023-05-18 16:54:50 -0300

    Create Curso TS

---
+++ Type-Script/typescript-curso-1/dist/js/app.js
@@ -0,0 +1,13 @@
+import { NegociacaoController } from './controllers/negociacao-controller.js';
+import { Negociacao } from './models/negociacao.js';
+
+
+//const controller = new NegociacaoController();
+const negociacao = new Negociacao(new Date(), 10,100)
+console.log(negociacao.volume);
+
+// const form = document.querySelector('.form');
+// form.addEventListener('submit', event => {
+//     event.preventDefault();
+//     controller.adiciona();
+// });
